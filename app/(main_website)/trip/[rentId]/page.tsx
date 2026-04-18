ce112bdf92e84325569e8eddc7991ca8e6a67d94
Adekoye Adewale <94379177+Adekoye-Adewale@users.noreply.github.com>
2026-04-18 19:46:55 +0100

    Create page.tsx

---
+++ app/(main_website)/trip/[rentId]/page.tsx
@@ -0,0 +1,12 @@
+
+export default async function page({
+        params,
+}: {
+        params: Promise<{ slug: string }>
+}) {
+        const { slug } = await params
+
+        return (
+                <div>page: {slug}</div>
+        )
+}
