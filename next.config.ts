c4c3f37346d606e454335e57e56274d405c28078
Senninseyi <seyi.oyebamiji@gmail.com>
2026-05-17 21:58:22 +0100

    fix: update API_BASE_URL for production and adjust next.config rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'files.example.com',
-			},
-			{ 
-				protocol: 'https', 
-				hostname: 'api.dicebear.com' 
-			},
-		],
-	},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "files.example.com",
+      },
+      {
+        protocol: "https",
+        hostname: "api.dicebear.com",
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/backend/:path*",
+        destination: "http://147.93.190.15:3000/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
